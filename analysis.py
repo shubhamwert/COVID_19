61393cd5c4eb41eb4189fa28528f77dab8a94797
Shubham <shubhamwert@gmail.com>
2020-04-20 19:25:14 +0530

    removed server files\n created new api endpoint and a basic web page

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
+import pygal 
 
-
-df=pd.read_csv('state_wise_corona_cases.csv')
+df=pd.read_csv('data/state_wise_corona_cases.csv')
 
 df.head()
 
@@ -9,4 +9,16 @@ df=df.drop([df.columns[0],df.columns[1]],axis=1)
 
 df.hist()
 a=df.plot.bar()
+df=df.sort_values(by=df.columns[1])
+
+def createSvgStateWise(n=0):
+    if(n==0):
+        n=df.shape[1]
+    p=df.tail(n)
+    a=pygal.Bar()
+    a.x_labels=p[p.columns[0]]
+    a.title="State Wise Plot of Covid 19"
+    for i in range(1,p.shape[1]):
+        a.add(p.columns[i],p[p.columns[i]])
+    return a
 
--- api.py
+++ api.py
@@ -2,8 +2,10 @@ import flask
 import pandas as pd
 import json
 import utils.data_scarping as ds
+import pygal 
+import analysis as an
 app=flask.Flask(__name__)
-with open('data/data.json) as f:
+with open('data/data.json') as f:
     data=json.load(f)
 @app.route('/state_wise_data/',methods=['GET'])
 def getData():
@@ -13,4 +15,14 @@ def getData():
 def updateData():
     ds.updateData()
     return json.dumps({'response':True})
+
+@app.route('/svg_state_wise_data',methods=['GET'])
+def svg_state_data():
+    a=an.createSvgStateWise()
+    return a.render_response()
+@app.route('/',methods=['GET'])
+def index():
+    a=an.createSvgStateWise(12)
+    return flask.render_template('index.html',graph_data=a.render(),table_data=a.render_table())
+
 app.run(host='0.0.0.0',port=5000)
